d5000df825f033bf0863c081a019185482e5f0a8
cabonilla <christian.bonilla01@gmail.com>
2021-08-06 07:41:51 -0700

    Basic flight effect working, extras next.

--- src/components/IconTwo/IconTwo.js
+++ src/components/IconTwo/IconTwo.js
@@ -1,14 +1,26 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 import { ReactComponent as Send } from '../../assets/send.svg'
 import './IconTwo.css'
 
 const IconTwo = () => {
+	const planeVariants = {
+		hidden: {scale: 1.5},
+		visible: { 
+			opacity: 1, 
+			y: ["-10vh", '-25vh', '-32vh', '-43vh', '-55vh'], 
+			x: ["12vw", '22vw', '35vw', '44vw', '59vw'], 
+			rotate: [10, 20, 10, 20, 10],
+			scale: [1.5, 1.4, 1.3, 1.2, 1.1],
+			transition: {duration: 10, type: "ease", stiffness: 10, repeat: Infinity}}
+	}
+
 	return (
 		<div className="second-container">
 			<h2>hello world</h2>
-			<div className="second-svg" id="send">
+			<motion.div variants={planeVariants} initial="hidden" animate="visible" className="second-svg" id="send">
 				<Send />
-			</div>
+			</motion.div>
 		</div>
 	)
 }
