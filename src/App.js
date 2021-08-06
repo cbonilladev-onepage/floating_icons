7b69a58d0de26f980d1fd39728fc119577655e3e
cabonilla <christian.bonilla01@gmail.com>
2021-08-05 21:18:29 -0700

    Started base for second animation. Shouldn't take too long.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import IconOne from './components/IconOne/IconOne';
+import IconTwo from './components/IconTwo/IconTwo';
 
 function App() {
   return (
     <div className="App">
       <IconOne/>
+      <IconTwo/>
     </div>
   );
 }
---
+++ src/components/IconTwo/IconTwo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ReactComponent as Send } from '../../assets/send.svg'
+import './IconTwo.css'
+
+const IconTwo = () => {
+	return (
+		<div className="second-container">
+			<h2>hello world</h2>
+			<div className="second-svg" id="send">
+				<Send />
+			</div>
+		</div>
+	)
+}
+
+export default IconTwo
