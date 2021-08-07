3cc4c160e001ed3be257810731b19b1d10225867
cabonilla <christian.bonilla01@gmail.com>
2021-08-07 15:19:31 -0700

    Added a bit more action to the second example. May add the birds.

--- src/components/IconTwo/IconTwo.js
+++ src/components/IconTwo/IconTwo.js
@@ -9,34 +9,43 @@ const IconTwo = () => {
 		hidden: {scale: 1.8},
 		visible: { 
 			opacity: 1, 
-			y: ["-10vh", '-25vh', '-32vh', '-43vh', '-55vh'], 
-			x: ["12vw", '22vw', '35vw', '44vw', '59vw'], 
-			rotate: [10, 20, 10, 20, 10],
-			scale: [1.5, 1.4, 1.2, 1.0, 0.8],
-			transition: {duration: 5, type: "linear", repeat: Infinity}}
+			y: ["12vh", "2vh", "-10vh", '-25vh', '-32vh', '-43vh', '-55vh', "-67vh", "-77vh", "-85vh"], 
+			x: ["-10vw", "2vw", "12vw", '22vw', '35vw', '44vw', '59vw', '68vw', '79vw', '85vw'], 
+			rotate: [10, 20, 10, 20, 10, 20, 10, 20, 10, 20],
+			scale: [2.1, 1.7, 1.5, 1.4, 1.2, 1.0, 0.8, 0.5, 0.2, 0.0],
+			transition: {duration: 10, type: "anticipate", repeat: Infinity}}
 	}
 
 	const cloudVariants = {
 		hidden: {x: -50},
-		visible: {x: '100vw', transition: {duration: 50, type: 'linear', repeat: Infinity}}
+		visible: {x: '100vw', transition: {duration: 80, type: 'linear', repeat: Infinity}}
 	}
 
 	const secondCloudVariants = {
 		hidden: {x: "-20vw", y: "40vh", scale: 1.3},
-		visible: {x: '100vw', transition: {duration: 80, type: 'linear', repeat: Infinity}}
+		visible: {x: '100vw', transition: {duration: 50, type: 'linear', repeat: Infinity}}
+	}
+
+	const thirdCloudVariants = {
+		hidden: {x: "-100px", y: "70vh", scale: 1.6},
+		visible: {x: '100vw', transition: {duration: 30, type: 'linear', repeat: Infinity}}
 	}
 
 	const uniqueId = "cloud"
 	const secondUniqueId = "cloud2"
+	const thirdUniqueId = "cloud3"
 
 	return (
 		<div className="second-container">
-			<h2>hello world</h2>
+			<h2 className="hello">hello world</h2>
 			<motion.div variants={cloudVariants} initial="hidden" animate="visible" className="hello">
 				<Cloud id={uniqueId}/>
 			</motion.div>
 			<motion.div variants={secondCloudVariants} initial="hidden" animate="visible" className="hello">
 				<Cloud id={secondUniqueId}/>
+			</motion.div>
+			<motion.div variants={thirdCloudVariants} initial="hidden" animate="visible" className="hello">
+				<Cloud id={thirdUniqueId}/>
 			</motion.div>
 			<motion.div variants={planeVariants} initial="hidden" animate="visible" className="second-svg" id="send">
 				<Send />
