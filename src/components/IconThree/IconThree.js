f225c23af88102f32545b0f6c9eff7a70be45454
cabonilla <christian.bonilla01@gmail.com>
2021-08-10 13:13:05 -0700

    Bowl of fruit, done. Just details now.

--- src/components/IconThree/IconThree.js
+++ src/components/IconThree/IconThree.js
@@ -1,29 +1,40 @@
 import { motion } from 'framer-motion';
 import React from 'react';
+import { ReactComponent as Apple } from '../../assets/apple.svg';
 import { ReactComponent as Banana } from '../../assets/bananas.svg';
-// import Bowl from '../../assets/bowl.png'
 import { ReactComponent as Bowl } from '../../assets/bowl.svg';
+import { ReactComponent as Orange } from '../../assets/fruit.svg';
+import { ReactComponent as Lime } from '../../assets/lime.svg';
+import { ReactComponent as Mango } from '../../assets/mango.svg';
 import './IconThree.scss';
 
 const IconThree = () => {
 	const bowlClass = "Bowl"
 	const bananaClass = "Banana"
-
-	const bananaVariant = {
-		hidden: {
-			x: "50px",
-			y: "-70px"
-		}
-	}
+	const appleClass = "Apple"
+	const orangeClass = "Orange"
+	const mangoClass = "Mango"
+	const limeClass = "Lime"
 
 	return (
 		<div class="third-container">
-			{/* <img className="Bowl" src={Bowl} alt="a bowl"/> */}
-			<motion.div className="icon_container">
+			<motion.div className="bowl_container">
 				<Bowl className={bowlClass} />
 			</motion.div>
-			<motion.div variants={bananaVariant} initial="hidden" className="icon_container">
+			<motion.div drag className="banana_container">
 				<Banana className={bananaClass} />
+			</motion.div>
+			<motion.div drag className="apple_container">
+				<Apple className={appleClass} />
+			</motion.div>
+			<motion.div drag className="orange_container">
+				<Orange className={orangeClass} />
+			</motion.div>
+			<motion.div drag className="mango_container">
+				<Mango className={mangoClass} />
+			</motion.div>
+			<motion.div drag className="lime_container">
+				<Lime className={limeClass} />
 			</motion.div>
 		</div>
 	)
